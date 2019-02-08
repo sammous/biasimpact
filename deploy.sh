58bdc5892b98c56299834de75313a73706f43f75
Sami Moustachir <moustachir.sami@gmail.com>
2019-02-08 17:07:52 +0800

    refactoring deploy

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+eval "$(ssh-agent -s)" # Start ssh-agent cache
+chmod 600 .travis/travis_rsa # Allow read access to the private key
+ssh-add .travis/travis_rsa # Add the private key to SSH
 
 ssh $USER@$IP <<EOF
   cd $DEPLOY_DIR
