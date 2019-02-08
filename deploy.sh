32d5595a6cd5639a052e78c1f485a2c9d85ad2aa
Sami Moustachir <moustachir.sami@gmail.com>
2019-02-08 17:29:23 +0800

    new deploy

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 eval "$(ssh-agent -s)" # Start ssh-agent cache
-chmod 600 .travis/travis_rsa # Allow read access to the private key
-ssh-add .travis/travis_rsa # Add the private key to SSH
+chmod 600 $TRAVIS_BUILD_DIR/.travis/travis_rsa # Allow read access to the private key
+ssh-add $TRAVIS_BUILD_DIR/.travis/travis_rsa # Add the private key to SSH
 
 ssh $USER@$IP <<EOF
   cd $DEPLOY_DIR
