f633f9c0754e0613f7e424bae7eea95c1e32e748
Sami Moustachir <moustachir.sami@gmail.com>
2019-02-08 17:33:57 +0800

    deploy host verification

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ eval "$(ssh-agent -s)" # Start ssh-agent cache
 chmod 600 $TRAVIS_BUILD_DIR/.travis/travis_rsa # Allow read access to the private key
 ssh-add $TRAVIS_BUILD_DIR/.travis/travis_rsa # Add the private key to SSH
 
-ssh $USER@$IP <<EOF
+ssh -o "StrictHostKeyChecking no" $USER@$IP <<EOF
   cd $DEPLOY_DIR
   git pull origin master
 EOF
